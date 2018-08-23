b76781890f4d3c49ae7ec726690e96fcb9963f3a
Mati Dastugue <matias.dastugue@mercadolibre.com>
2018-08-23 13:44:42 -0300

    add env variables for keys

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,9 +1,8 @@
+export const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+export const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN;
+export const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
+export const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
+export const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
+export const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID;
 
-export const FIREBASE_API_KEY = "AIzaSyAtBIka4d9aFYdBEpYJJS91z-M038Z4rOg";
-export const FIREBASE_AUTH_DOMAIN = "fulbito-904f4.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://fulbito-904f4.firebaseio.com";
-export const FIREBASE_PROJECT_ID = "fulbito-904f4";
-export const FIREBASE_STORAGE_BUCKET = "fulbito-904f4.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "301385923235";
-
-export const FACEBOOK_APP_ID = "2086851511569357";
+export const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
